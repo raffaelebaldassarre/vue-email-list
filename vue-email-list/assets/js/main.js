0fdd51285037d73c1168acb9e195f7c66959da0f
RaffaeleBaldassarre <raffaeleandrea.baldassarre@gmail.com>
2021-01-08 18:01:59 +0100

    Finish first

--- vue-email-list/assets/js/main.js
+++ vue-email-list/assets/js/main.js
@@ -1,6 +1,18 @@
 let app = new Vue ({
     el: "#app",
     data : {
+        emails: [
 
-    }
+        ]
+    },
+    mounted () {
+        for (let index = 0; index < 10; index++) {
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+          .then(response => {
+              //console.log(response);       
+              let emailRandom = response.data.response;
+              this.emails.push(emailRandom);
+          })
+        }
+      }
 });
