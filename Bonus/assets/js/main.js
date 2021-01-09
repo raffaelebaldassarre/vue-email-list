13463058c2ed10a7214281578cfa1b04391aa800
RaffaeleBaldassarre <raffaeleandrea.baldassarre@gmail.com>
2021-01-09 10:29:56 +0100

    Start Bonus

---
+++ Bonus/assets/js/main.js
@@ -0,0 +1,18 @@
+// Generate una Griglia 6x6, ad ogni click parte una richiesta AJAX che prende un numero random da 1 a 9.
+// Se è <= 5 il quadrato diventa giallo, se è > di 5 il quadrato diventa verde.
+// Il numero ottenuto appare al centro del quadrato
+
+
+
+let app = new Vue ({
+    el: "#app",
+    data : {
+      
+    },
+    mounted () {
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+          .then(response => {
+              //console.log(response);       
+          })
+      }
+});
